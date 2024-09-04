1328b005649ac854e8428a7b239d4a5ce8918882
pixiee534 <pixiee345@gmail.com>
2024-09-04 18:09:30 +0530

    first commit

---
+++ loops.js
@@ -0,0 +1,6 @@
+var vegetable=["Potato","Brocolli","Lady Finger","Corn","Spinach"];
+
+    for(i=0;i<vegetable.length;i++)
+        { var text = vegetable[i] + "<br>";
+        console.log(text);
+    }
---
+++ method.js
@@ -0,0 +1,13 @@
+var str="HeLlO"
+console.log (str.toUpperCase());
+console.log("<br>");
+console.log (str.toLowerCase());
+console.log("<br>");
+console.log (str.toUpperCase());
+console.log("<br>");
+console.log (str.toLowerCase());
+console.log("<br>");
+console.log (str.startsWith("H"));
+
+
+
