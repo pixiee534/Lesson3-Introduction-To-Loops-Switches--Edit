0bd0c61ea49ba28553a82e27927bc07e9aee42de
pixiee534 <pixiee345@gmail.com>
2024-09-08 10:44:26 +0530

    Homework

---
+++ Homework/homework.js
@@ -0,0 +1,6 @@
+var number =""
+(i = 1; i < 11; i++) {
+    number +=i+<br>
+    console.log("The random value is +Math.floor (Math.random() * 100));
+        var date = new Date();
+console.log("Today's Date is + date.getDate());
